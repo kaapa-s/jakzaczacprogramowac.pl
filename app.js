b9ebad8844823ef8f57c2194e58f39cd6ca27cf6
Kacper Sokołowski <kontakt@kaapa.pl>
2020-12-19 18:52:39 +0100

    Update filmów

--- app.js
+++ app.js
@@ -1,85 +1,122 @@
 (() => {
-    var episodes = [
-        {
-            title: "#19 - Zagraniczne oferty pracy dla programistów i programistek",
-            link: "https://youtu.be/vj2_cmIVRTo"
-        },
-        {
-            title: "#18 - Czy rekruter IT patrzy na wiek kandydata?",
-            link: "https://youtu.be/DNQhrLiA5o4"
-        },
-        {
-            title: "#17 - Skąd brać wiedzę i motywację do nauki? Front-end",
-            link: "https://youtu.be/Iqs6N1UZijs"
-        },
-        {
-            title: "#16 - Jak zmniejszyć stres na rozmowie kwalifikacyjnej?",
-            link: "https://youtu.be/uF5IvikYI78"
-        },
-        {
-            title: "#15 - Za stary na programowanie?",
-            link: "https://youtu.be/aPPkufI70HM"
-        },
-        {
-            title: "#14 - Skąd języki programowania wzięły swoje nazwy?",
-            link: "https://www.youtube.com/watch?v=omoIm8PpPzE"
-        },{
-            title: "#13 - Programujemy grę Sliding Puzzle w JavaScript",
-            link: "https://www.youtube.com/watch?v=dOxrZ3F_gu8"
-        },{
-            title: "#12 - 'Codzienność' branży IT okiem programisty",
-            link: "https://www.youtube.com/watch?v=0yR5raNYmC0"
-        },{
-            title: "#11 - Jak zwiększyć swoje szanse na zdobycie pracy jako junior developer?",
-            link: "https://www.youtube.com/watch?v=2GJnpgz5VrQ"
-        },{
-            title: "#10 - Gdzie szukać pracy jako programista i programistka w branży IT?",
-            link: "https://www.youtube.com/watch?v=2-09cjbaS8Q"
-        },{
-            title: "#9  - Popularne specjalizacje w programowaniu",
-            link: "https://www.youtube.com/watch?v=bYBrM9LMG5U"
-        },{
-            title: "#8  - Nauka programowania - moje błędy",
-            link: "https://www.youtube.com/watch?v=58etbJqMJR0"
-        },{
-            title: "#7  - Dzień z życia programisty. Praca zdalna.. z biura?",
-            link: "https://www.youtube.com/watch?v=R5FaS46guU0"
-        },{
-            title: "#6  - Jak kontynuować naukę programowania? Studia, kursy czy bootcampy?",
-            link: "https://www.youtube.com/watch?v=PJJc-psiimA"
-        },{
-            title: "#5  - Czy kariera programisty jest dla każdego?",
-            link: "https://www.youtube.com/watch?v=vGPyYdaoyXQ"
-        },{
-            title: "#4  - Od czego zacząć naukę programowania?",
-            link: "https://www.youtube.com/watch?v=NQzxGt-c9WE"
-        },{
-            title: "#3  - Dlaczego warto zacząć uczyć się programowania?",
-            link: "https://www.youtube.com/watch?v=_GWLayhyKSY"
-        },{
-            title: "#2  - Czym jest programowanie?",
-            link: "https://www.youtube.com/watch?v=sJVke1PKPDM"
-        },{
-            title: "#1  - Jak zostałem programistą?",
-            link: "https://www.youtube.com/watch?v=CHEGLDBS_aI"
-        },{
-            title: "#0  - Jak zacząć programować?",
-            link: "https://www.youtube.com/watch?v=yLvg_3HPobk"
-        }
-    ];
+  var episodes = [
+    {
+      title: '#22 - Nietypowe książki dla programistek i programistów',
+      link: 'https://www.youtube.com/watch?v=nXHYd5x3Bio',
+      embedLink: 'https://www.youtube.com/embed/nXHYd5x3Bio',
+      description:
+        'Kto powiedział, że programistom i programistkom przydadzą się tylko książki o kodzie?',
+    },
+    {
+      title: '#21 - Jak zrobić śnieg na stronę w JavaScript, HTML i CSS? cz. 2',
+      link: 'https://www.youtube.com/watch?v=2U-hHMMqS8E',
+    },
+    {
+      title: '#20 - Jak zrobić śnieg na stronę w JavaScript, HTML i CSS? cz. 1',
+      link: 'https://www.youtube.com/watch?v=u9OvTqC20CU',
+    },
+    {
+      title: '#19 - Zagraniczne oferty pracy dla programistów i programistek',
+      link: 'https://youtu.be/vj2_cmIVRTo',
+    },
+    {
+      title: '#18 - Czy rekruter IT patrzy na wiek kandydata?',
+      link: 'https://youtu.be/DNQhrLiA5o4',
+    },
+    {
+      title: '#17 - Skąd brać wiedzę i motywację do nauki? Front-end',
+      link: 'https://youtu.be/Iqs6N1UZijs',
+    },
+    {
+      title: '#16 - Jak zmniejszyć stres na rozmowie kwalifikacyjnej?',
+      link: 'https://youtu.be/uF5IvikYI78',
+    },
+    {
+      title: '#15 - Za stary na programowanie?',
+      link: 'https://youtu.be/aPPkufI70HM',
+    },
+    {
+      title: '#14 - Skąd języki programowania wzięły swoje nazwy?',
+      link: 'https://www.youtube.com/watch?v=omoIm8PpPzE',
+    },
+    {
+      title: '#13 - Programujemy grę Sliding Puzzle w JavaScript',
+      link: 'https://www.youtube.com/watch?v=dOxrZ3F_gu8',
+    },
+    {
+      title: "#12 - 'Codzienność' branży IT okiem programisty",
+      link: 'https://www.youtube.com/watch?v=0yR5raNYmC0',
+    },
+    {
+      title:
+        '#11 - Jak zwiększyć swoje szanse na zdobycie pracy jako junior developer?',
+      link: 'https://www.youtube.com/watch?v=2GJnpgz5VrQ',
+    },
+    {
+      title:
+        '#10 - Gdzie szukać pracy jako programista i programistka w branży IT?',
+      link: 'https://www.youtube.com/watch?v=2-09cjbaS8Q',
+    },
+    {
+      title: '#9  - Popularne specjalizacje w programowaniu',
+      link: 'https://www.youtube.com/watch?v=bYBrM9LMG5U',
+    },
+    {
+      title: '#8  - Nauka programowania - moje błędy',
+      link: 'https://www.youtube.com/watch?v=58etbJqMJR0',
+    },
+    {
+      title: '#7  - Dzień z życia programisty. Praca zdalna.. z biura?',
+      link: 'https://www.youtube.com/watch?v=R5FaS46guU0',
+    },
+    {
+      title:
+        '#6  - Jak kontynuować naukę programowania? Studia, kursy czy bootcampy?',
+      link: 'https://www.youtube.com/watch?v=PJJc-psiimA',
+    },
+    {
+      title: '#5  - Czy kariera programisty jest dla każdego?',
+      link: 'https://www.youtube.com/watch?v=vGPyYdaoyXQ',
+    },
+    {
+      title: '#4  - Od czego zacząć naukę programowania?',
+      link: 'https://www.youtube.com/watch?v=NQzxGt-c9WE',
+    },
+    {
+      title: '#3  - Dlaczego warto zacząć uczyć się programowania?',
+      link: 'https://www.youtube.com/watch?v=_GWLayhyKSY',
+    },
+    {
+      title: '#2  - Czym jest programowanie?',
+      link: 'https://www.youtube.com/watch?v=sJVke1PKPDM',
+    },
+    {
+      title: '#1  - Jak zostałem programistą?',
+      link: 'https://www.youtube.com/watch?v=CHEGLDBS_aI',
+    },
+    {
+      title: '#0  - Jak zacząć programować?',
+      link: 'https://www.youtube.com/watch?v=yLvg_3HPobk',
+    },
+  ];
 
-    episodes.shift();
+  const lastEpisode = episodes.shift();
 
-    episodes.forEach(episode => {
-        var container = document.createElement("div"); 
-        var link = document.createElement("a");
-        link.innerText = episode.title;
-        link.setAttribute("href", episode.link);
-        link.setAttribute("target", "_blank");
-        container.classList.add("episode");
-        container.appendChild(link);
+  document.querySelector('#main-video-title').innerText = lastEpisode.title;
+  document.querySelector('#main-video-iframe').src = lastEpisode.embedLink;
 
-        document.querySelector(".epiosdes").appendChild(container);
+  document.querySelector('#main-video-description').innerText =
+    lastEpisode.description;
 
-    })
-})()
+  episodes.forEach((episode) => {
+    var container = document.createElement('div');
+    var link = document.createElement('a');
+    link.innerText = episode.title;
+    link.setAttribute('href', episode.link);
+    link.setAttribute('target', '_blank');
+    container.classList.add('episode');
+    container.appendChild(link);
+
+    document.querySelector('.epiosdes').appendChild(container);
+  });
+})();
